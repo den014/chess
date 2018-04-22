cf12fbc9bd464fc2c07755b90b1374e80dd8e398
Deniss Aleksejenko <denissaleksejenko@gmail.com>
2018-04-23 01:34:40 +0300

    change path

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from '$store/configureStore';
-import Board from '$components/Board/Board';
+import configureStore from './store/configureStore';
+import Board from './components/Board/Board';
 
 import './index.scss';
 
